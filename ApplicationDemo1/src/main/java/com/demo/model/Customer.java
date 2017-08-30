0aa18b2095ff0469291ebb025da05d7d08721b59
baid <babdenbi@gmail.com>
2017-08-30 16:20:28 +0100

    Add - Java Bean Customer

---
+++ ApplicationDemo1/src/main/java/com/demo/model/Customer.java
@@ -0,0 +1,69 @@
+package com.demo.model;
+
+public class Customer {
+	
+	private long id;
+	private String name;
+	private String prenom;
+	private String code;
+	private String adresse1;
+	private String adresse2;
+	private String adresse3;
+	private String ville;
+	private String pays;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getAdresse1() {
+		return adresse1;
+	}
+	public void setAdresse1(String adresse1) {
+		this.adresse1 = adresse1;
+	}
+	public String getAdresse2() {
+		return adresse2;
+	}
+	public void setAdresse2(String adresse2) {
+		this.adresse2 = adresse2;
+	}
+	public String getAdresse3() {
+		return adresse3;
+	}
+	public void setAdresse3(String adresse3) {
+		this.adresse3 = adresse3;
+	}
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	public String getPays() {
+		return pays;
+	}
+	public void setPays(String pays) {
+		this.pays = pays;
+	}
+
+}
